df9bf0f950d49b8b9cf8e4ca8294bf05e41c77b2
劉天佑 <gn00595067@gmail.com>
2016-01-04 08:39:38 +0800

    position industry

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,17 @@
 class TopicsController < ApplicationController
-	def index  #
-		@topics=Topic.all
+	def index  
 		@industries=Industry.all
+
+		if params[:p]
+			@position=Position.find(params[:p])
+			@topics=@position.topics
+			if params[:i]
+				@industry=Industry.find(params[:i])
+				@topics=@topics.where(industry_id:params[:i])
+
+			end
+		else
+			@topics=Topic.all
+		end
 	end
 end
