3454db1f176a571565b271dd1dbf62e777e8bab4
SHOUTSE <frankchang1983@gmail.com>
2016-01-19 20:48:30 +0800

    admin can add crud drafts...can crud topics

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -26,11 +26,26 @@ class DraftsController < ApplicationController
 
 	end
 
+	def edit
+		@draft = Draft.find(params[:id])
+	end
+
+	def update
+
+		@draft = Draft.find(params[:id])
+			if @draft.update(draft_params)
+    	  flash[:notice] = "編輯成功"
+    	  redirect_to admin_draft_url
+    	else
+      render :action => :edit
+    end
+	end
+
 	protected
 
 	def draft_params
 
-		params.require(:draft).permit(:name,:content,:istrue,:privacy,:industry_id,:position_id,:working_time,:answer1)
+		params.require(:draft).permit(:name,:content,:istrue,:privacy,:industry_id,:position_id,:working_time,:answer1, :bootsy_image_gallery_id)
 
 	end
 
--- app/models/draft.rb
+++ app/models/draft.rb
@@ -3,6 +3,7 @@ class Draft < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :industry
 	belongs_to :position
+  include Bootsy::Container
 
-  validates_presence_of :name, :position_id, :industry_id, :content, :duration
+  validates_presence_of :name, :position_id, :industry_id, :content, :working_time
 end
