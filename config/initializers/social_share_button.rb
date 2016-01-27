02b26c3b9fcea53decf5f5812c73e641dc5184a0
劉天佑 <gn00595067@gmail.com>
2016-01-27 12:16:48 +0800

    頁面修改

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,4 +1,4 @@
 SocialShareButton.configure do |config|
-	# config.allow_sites = %w(twitter facebook google_plus email)
-  config.allow_sites = %w(facebook)
+	config.allow_sites = %w(twitter facebook google_plus email)
+  
 end
